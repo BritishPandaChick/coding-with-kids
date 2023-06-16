2cff8a903f0ebed103e553ccb0e49a542a5fec13
britishpandachick <sbartley1080@gmail.com>
2023-06-15 21:40:20 -0400

    Add phase 2 code for snowman game

--- level-m/snowman/snowman.py
+++ level-m/snowman/snowman.py
@@ -12,6 +12,16 @@ def init():
     guessed = ''
     
 def main():
+    global guessed
     init()
+    print(" ".join(l for l in display_word))
+    while True:
+        letter = input("Guess a letter: ")
+        guessed += letter
+        for i in range(len(word)):
+            if word[i] == letter:
+                display_word[i] = letter
+        print(" ".join(l for l in display_word))
+        print("Letters guessed: "+guessed)
 
 main()
