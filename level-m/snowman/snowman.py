cfdcc22572121a2303cd4d962c2c73efe087dbe1
britishpandachick <sbartley1080@gmail.com>
2023-06-15 22:16:50 -0400

    Add phase 4 code for snowman game

--- level-m/snowman/snowman.py
+++ level-m/snowman/snowman.py
@@ -10,18 +10,35 @@ def init():
     display_word = ["_"] * len(word)
     guesses = 6
     guessed = ''
+
+def word_guessed():
+    return "_" not in display_word
     
 def main():
-    global guessed
+    global guesses, guessed
     init()
     print(" ".join(l for l in display_word))
-    while True:
+    while not word_guessed() and guesses > 0:
         letter = input("Guess a letter: ")
+        if len(letter) != 1:
+            print('Please enter one letter at a time')
+            continue
+        elif letter in guessed:
+            print('You already tried "'+letter+'" guess again')
+            continue
         guessed += letter
-        for i in range(len(word)):
-            if word[i] == letter:
-                display_word[i] = letter
+        if letter not in word:
+            guesses -= 1
+        else:
+            for i in range(len(word)):
+                if word[i] == letter:
+                    display_word[i] = letter
         print(" ".join(l for l in display_word))
         print("Letters guessed: "+guessed)
+        print("Guesses left: ()".format(guesses))
+    if guesses > 0:
+        print("You guessed it!")
+    else:
+        print("Better luck next time")
 
 main()
