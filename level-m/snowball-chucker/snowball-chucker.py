373bc3fd2d527ac8fd7d741f2d379a9bcb2bef4c
britishpandachick <sbartley1080@gmail.com>
2023-07-14 15:28:43 -0400

    Add code files. Add phase one code

---
+++ level-m/snowball-chucker/snowball-chucker.py
@@ -0,0 +1,15 @@
+stage.set_background_color("grey")
+stage.disable_all_walls()
+
+#setup sprites and bases 
+base1 = codesters.Circle(-225, -225, 200, "brown")
+base2 = codesters.Circle(225, 225, 200, "brown")
+player = codesters.Sprite("robot", -200, -200)
+player.set_size(0.5)
+computer = codesters.Sprite("enemy", 210, 210)
+computer.set_size(0.5)
+
+#setup game variables 
+player.lives = 5
+computer.lives = 5
+game_over = False
