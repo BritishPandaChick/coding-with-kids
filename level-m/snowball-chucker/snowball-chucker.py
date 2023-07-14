a625241e8f78dc766e6f83b7601676fcb632eb90
britishpandachick <sbartley1080@gmail.com>
2023-07-14 15:36:10 -0400

    Add phase 2 code

--- level-m/snowball-chucker/snowball-chucker.py
+++ level-m/snowball-chucker/snowball-chucker.py
@@ -13,3 +13,21 @@ computer.set_size(0.5)
 player.lives = 5
 computer.lives = 5
 game_over = False
+
+# calculate vector from player to point licked, vectors are calculated by subtracting the second point by the first point (difference)
+def calculate_vector(mouseX, mouseY):
+    vector_x = mouseX - player.get_x()
+    vector_y = mouseY - player.get_y()
+    return [vector_x, vector_y] # return the x and y component of the vector as a list 
+
+#gets the magnitude of a vector; formula here where c is the magnitude: c^2 = a^2 + b^2 => c = sqrt(a^2 + b^2)
+def magnitude(vector):
+    return math.sqrt(vector[0] * vector[0] + vector[1] * vector[1])
+
+#normalize a vector by dividing by the magnitude 
+def divide(vector, number):
+    if number == 0:
+        return vector
+    normX = vector[0] / number #normalize x component 
+    normY = vector[1] / number #normalize y component 
+    return [normX, normY]
