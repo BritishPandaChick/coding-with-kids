04f82a043470c678a2fa1eb34864525e9fce0144
britishpandachick <sbartley1080@gmail.com>
2023-08-10 01:52:51 -0400

    Add phase two code

--- level-m/hangman/hangman.py
+++ level-m/hangman/hangman.py
@@ -7,3 +7,8 @@ MAX_GUESSES = 6
 # Global Variables
 secret_word = ''
 letters_guessed = []
+
+def get_word():
+    words = ["apple", "orange", "python", "happy", "birthday", "random", "life", "string", "list"]
+    word = random.choice(words)
+    return word
