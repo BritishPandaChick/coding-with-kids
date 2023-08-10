517b9cd73919bbb14a5c4cadcca1309528ffa738
britishpandachick <sbartley1080@gmail.com>
2023-08-10 01:56:11 -0400

    Add phase 3 code

--- level-m/hangman/hangman.py
+++ level-m/hangman/hangman.py
@@ -12,3 +12,13 @@ def get_word():
     words = ["apple", "orange", "python", "happy", "birthday", "random", "life", "string", "list"]
     word = random.choice(words)
     return word
+
+def word_guessed():
+    # Set up variables 
+    global secret_word, letters_guessed
+
+    for letter in secret_word:
+        if not (letter in letters_guessed):
+          return False
+    return True
+    
