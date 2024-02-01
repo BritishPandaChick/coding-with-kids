bc348b25dbd4648e374c6e0867bc25cc5100f22a
britishpandachick <sbartley1080@gmail.com>
2024-02-01 17:06:24 -0500

    add phase 3 code to bubble pop

--- level-m/bubble-pop/bubble-pop.py
+++ level-m/bubble-pop/bubble-pop.py
@@ -28,6 +28,47 @@ def reset_ball():
     ball.go_to(0, -210)
     ball.set_color(random.choice(colors))
 
+# Player controls 
+def left_key():
+    if not game_over:
+        arrow.set_rotation(arrow.get_rotation()+5)
+stage.event_key("left", left_key)
+
+def right_key():
+    if not game_over:
+        arrow.set_rotation(arrow.get_rotation()-5)
+stage.event_key("right", right_key)
+
+def up_key():
+    global moving, shot_count, color
+    if not moving and not game_over:
+        color = ball.get_color()
+        ball.set_rotation(arrow.get_rotation())
+        moving = True 
+        while moving:
+            ball.move_forward(8)
+            stage.wait(.02)
+            # create side walls 
+            if ball.get_x() > 240 or ball.get_x() < -240:
+                ball.set_rotation(180-ball.get_rotation())
+        #move balls down the screen ever 5 shots 
+        shot_count += 1
+        if shot_count%5 == 0:
+            move_bubbles()
+        reset_ball()
+stage.event_key("up", up_key)
+
+#move bubbles down the screen 
+def move_bubbles():
+    global game_over
+    back_board.set_y(back_board.get_y()-15)
+    for bubble in bubbles:
+        bubble.set_y(bubble.get_y()-15)
+        
+    if bubbles[len(bubbles)-1].get_y() < -170:
+        text.set_text("Game Over")
+        game_over = True
+
 def main():
     reset_ball()
     create_bubbles()
