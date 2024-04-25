c6d9646179f3b5c181bc5049e5e714fccd38cc2b
britishpandachick <sbartley1080@gmail.com>
2024-04-25 00:54:09 -0400

    Add phase 3 code for trivia quiz

--- level-m/trivia-quiz/trivia-quiz-phase3.py
+++ level-m/trivia-quiz/trivia-quiz-phase3.py
@@ -19,6 +19,10 @@ answers = []
 # each element is a reference to a text sprite
 correct_answers = []
 
+# Added in Phase 3
+question_num = 0
+score = 0
+
 # Phase 2 - adding questions to the game
 def create_questions():
     #question 1
@@ -35,7 +39,27 @@ def create_questions():
     questions.append("What  is 'pixel' short for?")
     answers.append(["pixel - it's not \nshort for anything", "Edward Pixelli", "pixelized point", "picture element"])
     correct_answers.append(d)
+
+# Phase 3 - ask questions and checking the answer 
+def ask_questions():
+    question.set_text(questions[question_num])
+    a.set_text("A: "+answers[question_num][0])
+    b.set_text("B: "+answers[question_num][1])
+    c.set_text("C: "+answers[question_num][2])
+    d.set_text("D: "+answers[question_num][3])
+    
+def answer_click(choice):
+    global score
+    if choice is correct_answers[question_num]:
+        display.set_text("Correct!")
+        score += 1
+    else:
+        display.set_text("Incorrect")
+
+for choice in choices:
+    choice.event_click(answer_click)
     
 def main():
     create_questions()
+    ask_questions() # add phase 3
 main()
